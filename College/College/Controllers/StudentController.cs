1b3b177b5bae223f54f61a10ef3dfe2630ed2841
TS1A0103 <vamsi.krishna@mail.techsemble.us>
2026-02-17 15:17:04 -0600

    Intial WebAPI

---
+++ College/College/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace College.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        //endpoint
+        [HttpGet]
+        public string GetStudentName()
+        {
+            return  "Student name 1";
+        }
+    }
+}
