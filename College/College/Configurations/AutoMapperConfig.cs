1d8d9023de7045bde63058af40be91b2bfe4a19e
TS1A0103 <vamsi.krishna@mail.techsemble.us>
2026-02-21 21:02:03 -0500

    Automapper with different property operations

--- College/College/Configurations/AutoMapperConfig.cs
+++ College/College/Configurations/AutoMapperConfig.cs
@@ -14,6 +14,14 @@ namespace College.Configurations
 
             //To Map the classes as above in sinle line
             CreateMap<StudentDTO, Student>().ReverseMap();
+            //Config for differently named property
+            //CreateMap<StudentDTO, Student>().ReverseMap().ForMember(n =>n.Name, opt => opt.MapFrom(x =>x.StudentName)).;
+
+            //Config for Ignoring property
+            //CreateMap<StudentDTO, Student>().ReverseMap().ForMember(n => n.StudentName, opt => opt.Ignore());
+
+            //Config for making meaningful otput on null values
+            //CreateMap<StudentDTO, Student>().ReverseMap().AddTransform<string>(n => string.IsNullOrEmpty(n) ? "No Address Found" : n);
         }
     }
 }
